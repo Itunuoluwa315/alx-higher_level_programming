3ca4bf5a8169c2235ea0715fb80ddf38500ca367
itunuoluwa315 <olatunjisamuel315@gmail.com>
2023-03-15 19:37:10 -0700

    task update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print('{:d}'.format(i))
+def square_matrix_simple(matrix=[]):
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
